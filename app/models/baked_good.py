bfbd9eaf6f381c4f4452e1f9de2ce1c55211a7ca
kamaukimani <paulkimys285@gmail.com>
2026-09-17 04:20:27 +0300

    set up BakedGood model

---
+++ app/models/baked_good.py
@@ -0,0 +1,20 @@
+from app.db import db
+from sqlalchemy.orm import Mapped,mapped_column,relationship
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import func,ForeignKey
+from datetime import datetime
+
+class BakedGood(db.Model,SerializerMixin):
+    __tablename__="baked_goods"
+
+    id:Mapped[int]=mapped_column(primary_key=True)
+    name:Mapped[str]
+    price:Mapped[int]
+    created_at:Mapped[datetime]=mapped_column(server_default=func.now())
+    updated_at:Mapped[datetime]=mapped_column(server_default=func.now(),onupdate=func.now())
+
+    bakery_id:Mapped[int]=mapped_column(ForeignKey("bakeries.id"))
+
+
+    def __repr__(self):
+        return f"<BakedGood ({self.id}) {self.name}, {self.price}>"
