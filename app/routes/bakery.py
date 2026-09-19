6bd74a633f669356dfc3c3ddc02df5263905dd02
kamaukimani <paulkimys285@gmail.com>
2026-09-19 15:45:58 +0300

    handle non-existing records

--- app/routes/bakery.py
+++ app/routes/bakery.py
@@ -16,7 +16,14 @@ def bakeries():
 @bakery_bp.route('/<int:id>')
 def bakery_by_id(id):
     bakery=Bakery.query.filter(Bakery.id == id).first()
-    bakery_dict=bakery.to_dict()
+
+    if bakery is None:
+        response_body={
+            "message":"The record does not exist in our database!!!!"
+        }
+        return make_response(response_body,404)
+
+    bakery_dict=bakery.to_dict(rules=("-baked_goods",))
 
     response=make_response(
         bakery_dict,
